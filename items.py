a87cb83aee64507820c1429fc50e9d8d8308e2bf
Alvaro Torres <torrestal@gmail.com>
2019-11-25 14:54:37 +0100

    added minerals

--- items.py
+++ items.py
@@ -6,6 +6,7 @@ import abc
 class Item:
     vel_mod = None
     color = None
+    radius = None
 
     @abc.abstractmethod
     def is_instantiable(self):
@@ -19,8 +20,26 @@ class Item:
         pygame.draw.circle(surface, self.color, self.pos.int(), 5, 0)
 
 
+class Mineral(Item):
+    color = 255, 150, 100
+    radius = 20
+
+    def __init__(self, pos: Vector):
+        super().__init__(pos)
+        self.points = [point.int() for point in (
+            pos + Vector(Mineral.radius, Mineral.radius), pos + Vector(-Mineral.radius, Mineral.radius),
+            pos + Vector(-Mineral.radius, -Mineral.radius), pos + Vector(Mineral.radius, -Mineral.radius),)]
+
+    def is_instantiable(self):
+        return True
+
+    def draw(self, surface: pygame.Surface):
+        pygame.draw.polygon(surface, self.color, self.points)
+
+
 class Tree(Item):
     color = 100, 200, 10
+    radius = 5
 
     def is_instantiable(self):
         return True
