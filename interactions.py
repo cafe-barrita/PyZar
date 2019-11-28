50e144ab3c46c2db103b1be3123ec9f314deea48
Alvaro Torres <torrestal@gmail.com>
2019-11-28 12:47:50 +0100

    minor improvement

--- interactions.py
+++ interactions.py
@@ -35,25 +35,20 @@ class Interaction:
 
     @staticmethod
     def check_obstacles(characters: List[Character], obstacles: List[Obstacle]):
-        angle_addition = math.pi / 16
+        increment = angle_addition = 0.39  # pi/8
         for character in characters:
             if character.director_vector:
                 for obstacle in obstacles:
                     d = abs(obstacle.pos - character.pos)
                     if d < (obstacle.radius + character.sight_radius):
                         # FIXME que mire posiciones intermedias pues si es pequeño atraviesa
-                        future_pos = character.pos + character.director_vector.unit() * character.sight_radius
-                        if obstacle.is_point_inside(future_pos):
+                        while obstacle.is_point_inside(
+                                character.pos + character.director_vector.unit() * character.sight_radius):
+                            # FIXME si entra es while true por que nada cambia la condicion de entrada
                             # FIxme IMPROVE THSI
-                            salir = False
-                            polar_unit_dir_vector = character.director_vector.unit().to_polar()
-                            while not salir:
-                                right, left = Interaction.get_new_unit_vectors(polar_unit_dir_vector, angle_addition)
-                                if not obstacle.is_point_inside(character.pos + right * character.sight_radius):
-                                    character.destination = character.pos + right * character.sight_radius
-                                    salir = True
-                                elif not obstacle.is_point_inside(character.pos + left * character.sight_radius):
-                                    character.destination = character.pos + left * character.sight_radius
-                                    salir = True
-                                else:
-                                    angle_addition += math.pi / 16
+                            for vector in Interaction.get_new_unit_vectors(character.director_vector.unit().to_polar(),
+                                                                           angle_addition):
+                                if not obstacle.is_point_inside(character.pos + vector * character.sight_radius):
+                                    character.destination = character.pos + vector * character.sight_radius
+                                    return
+                            angle_addition += increment
