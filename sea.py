222dbf80bbd8dbee8dcd470cefa9f7a2deb397f8
Alvaro Torres <torrestal@gmail.com>
2019-12-02 15:33:22 +0100

    created sea with perlin noise algorithm

--- sea.py
+++ sea.py
@@ -2,93 +2,28 @@ import random
 
 import pygame
 from vector_2d import Vector
-import math
-import pprint
+
+from perlin import PerlinNoiseFactory
 
 
 class Sea:
-    DIST = 6
-    # RADIO = int(DIST / math.sqrt(3))
-    RADIO = 3
-    CARD = 'nose'
-    DIRECTIONS = {
-        'n': Vector(0, -DIST),
-        'o': Vector(-DIST, 0),
-        's': Vector(0, DIST),
-        'e': Vector(DIST, 0),
-        'p': Vector(0, 0),
-    }
+    def __init__(self, res):
+        self.pos = Vector(400, 400)
+        perlin = PerlinNoiseFactory(2, 3)
+        width = int(random.gauss(100, 2))
+        height = int(random.gauss(100, 2))
+        noise = [[perlin(x / width, y / height) for y in range(height)] for x in range(width)]
+        mini = min(min(row) for row in noise)
+        noise = [[e + abs(mini) for e in row] for row in noise]
+        maxi = max(max(row) for row in noise)
+        self.noise = [[int(255 * e / maxi) for e in row] for row in noise]
+        print(noise)
 
-    def __init__(self, resolution):
-        # self.dict = {key: None for key in random.sample(Sea.CARD, k=4)}
-        # self.set = set()
-        # dic = self.dict
-        # while dic:
-        #     for key in dic:
-        #         dic[key] = {sub_key: None for sub_key in
-        #                     random.sample(Sea.CARD, k=min(4, int(random.gauss(mu=5.5, sigma=1))))}
-        #     dic = dic[key]
-        # self.clean()
-
-        # pprint.pprint(self.dict)
-
-        CARD = ('n', 'o', 's', 'e', 'p')
-        output = {'n', 'o', 's', 'e'}
-        for _ in range(20):
-            output_copy = output.copy()
-            for element in output_copy:
-                if element[-1] != 'p':
-                    output.discard(element)
-                    for element2 in random.sample(CARD, k=max(0, min(4, int(random.gauss(mu=2.6, sigma=1))))):
-                        output.add(element + element2)
-        print(output)
-        self.output = output
-        self.vector_set = set()
-        self.clean()
-
-    def clean(self):
-        # dic = self.dict
-        # vector = Vector(400, 400)
-        # self.set.add(vector)
-        # while dic:
-        #     for key in dic:
-        #         vector += Sea.DIRECTIONS[key]
-        #         self.set.add(vector)
-        #     dic = dic[key]
-        vector = Vector(400, 400)
-        vector_set = {vector}
-        for element in self.output:
-            vector = Vector(400, 400)
-            for letter in element:
-                vector += Sea.DIRECTIONS[letter]
-                vector_set.add(vector)
-
-        self.vector_set = vector_set
-        print(vector_set)
-
-    def draw(self, surface, t):
-        print(t)
-        for vector in self.vector_set:
-            pygame.draw.circle(surface, (0, 50, 255), vector.int(), Sea.RADIO)
+    def draw(self, screen, t):
+        for x, row in enumerate(self.noise):
+            for y, g in enumerate(row):
+                pygame.draw.circle(screen, (0, g, 0), (x, y), 1)
 
 
 if __name__ == '__main__':
-    CARD = ('n', 'o', 's', 'e', 'p')
-    output = {'n', 'o', 's', 'e'}
-    for _ in range(4):
-        output_copy = output.copy()
-        for element in output_copy:
-            if element[-1] != 'p':
-                output.discard(element)
-                for element2 in random.sample(CARD, k=random.randrange(6)):
-                    output.add(element + element2)
-    print(output)
-
-    vector = Vector(400, 400)
-    vector_set = {vector}
-    for element in output:
-        for letter in element:
-            vector += Sea.DIRECTIONS[letter]
-            vector_set.add(vector)
-
-    print(vector_set)
+    sea = Sea('')
