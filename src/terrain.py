fada8aa4010fa089ffcd3c85bcf97c9270d9354e
Alvaro Torres <torrestal@gmail.com>
2019-12-04 16:32:50 +0100

    added corners to the isoline

--- src/terrain.py
+++ src/terrain.py
@@ -19,24 +19,60 @@ class Terrain:
         self.noise -= self.noise.min()
         self.noise *= 255 / self.noise.max()
         # TODO meter en un diccionario
+        self.dict_is_corner_x = {0: {0: (0, 0),
+                                     self.res[1] - 1: (0, self.res[1] - 1),
+                                     },
+                                 self.res[0] - 1: {0: (self.res[0] - 1, 0),
+                                                   self.res[1] - 1: (self.res[0] - 1, self.res[1] - 1),
+                                                   },
+                                 }
+        self.dict_is_corner_y = {0: {0: (0, 0),
+                                     self.res[0] - 1: (self.res[0] - 1, 0),
+                                     },
+                                 self.res[1] - 1: {0: (0, self.res[1] - 1),
+                                                   self.res[0] - 1: (self.res[0] - 1, self.res[1] - 1),
+                                                   },
+                                 }
         self.isolines = []
         self.calc_contours()
 
     def calc_contours(self):
-        contours = measure.find_contours(self.noise, 125)
+        contours = measure.find_contours(self.noise, 90, fully_connected='high', positive_orientation='high')
         self.isolines = [[(int(x), int(y)) for x, y in list(contour)] for contour in contours]
-        ...
+        self.low_isolines = [[(int(x), int(y)) for x, y in list(contour)] for contour in
+                             measure.find_contours(self.noise, 90, fully_connected='low', positive_orientation='low')]
 
-    def draw(self, screen, t):
-        print(t)
+        # add corners
+        # FIXME it do not add corners if isoline touches oposite sides
+        for i, isoline in enumerate(self.low_isolines):
+            print(isoline)
+            try:
+                point = self.dict_is_corner_x[isoline[0][0]][isoline[-1][1]]
+            except KeyError:
+                try:
+                    point = self.dict_is_corner_y[isoline[0][1]][isoline[-1][0]]
+                except KeyError:
+                    pass
+                else:
+                    print('RinconY')
+                    isoline.append(point)
+            else:
+                print('RinconX')
+                isoline.append(point)
+
+    def draw(self, screen, t, c: bool):
+        # print(t)
         for x in range(0, self.res[0], self.tile_side):
             for y in range(0, self.res[1], self.tile_side):
                 g = self.noise[x // self.tile_side][y // self.tile_side]
                 pygame.draw.circle(screen, (0, g, 255 - g), (x, y), self.tile_side)
                 # pygame.draw.circle(screen, (g, g, 255), (x, y), self.tile_side)
 
-        for isoline in self.isolines:
-            pygame.draw.polygon(screen, (200, 0, 0), isoline, 1)
+        if c:
+            for isoline in self.isolines:
+                pygame.draw.polygon(screen, (200, 0, 0), isoline, 1)
+            for isoline in self.low_isolines:
+                pygame.draw.polygon(screen, (100, 100, 0), isoline)
 
 
 if __name__ == '__main__':
